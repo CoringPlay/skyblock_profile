d555c1cf0d130f8accb37dff86c3bd91dfccb6bb
CoringPlay <61494265+CoringPlay@users.noreply.github.com>
2023-11-13 12:50:35 +1000

    Improve pet details display in profile.php
    Integrate Montserrat font, apply it to the entire website

--- profile.php
+++ profile.php
@@ -6,23 +6,37 @@
     <title>Profile Viewer</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="pet-styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"></noscript>
+    <style>
+        .pet-details {
+            padding: 10px;
+            border: 1px solid #ddd;
+            margin-top: 10px;
+            background-color: #f9f9f9;
+        }
+
+        .pet-details strong {
+            color: #333;
+        }
+    </style>
 </head>
 <body>
 <?php
 if (isset($_GET['playerName'])) {
     $playerName = $_GET['playerName'];
     $apiUrl = "https://sky.shiiyu.moe/api/v2/profile/{$playerName}";
-    
+
     // Получаем JSON-данные
     $json = file_get_contents($apiUrl);
-    
+
     // Преобразуем JSON в массив
     $data = json_decode($json, true);
-    
+
     if ($data) {
         // Отображаем профиль
         echo "<h1>Профиль игрока: {$playerName}</h1>";
-        
+
         // Выводим массив profiles
         echo "<h2>Выберите профиль:</h2>";
         echo "<ul>";
@@ -31,7 +45,7 @@ if (isset($_GET['playerName'])) {
             echo "<li><a href='profile.php?playerName={$playerName}&profile={$profile['profile_id']}'>{$profile['cute_name']}</a></li>";
         }
         echo "</ul>";
-        
+
         // Проверяем, есть ли выбранный профиль в URL
         if (isset($_GET['profile'])) {
             $selectedProfileId = $_GET['profile'];
@@ -64,7 +78,7 @@ if (isset($_GET['playerName'])) {
                         usort($profile['raw']['pets'], 'comparePetsByRarity');
 
                         // Выводим питомцев
-                        echo "<h3>Информация из массива 'pets':</h3>";
+                        echo "<h3>Информация о питомцах:</h3>";
                         echo "<div class='pet-container'>";
                         foreach ($profile['raw']['pets'] as $pet) {
                             // Проверяем, есть ли нужные ключи в данных о питомце
@@ -100,12 +114,11 @@ if (isset($_GET['playerName'])) {
                             }
 
                             echo "<div class='pet-item {$rarityClass}'>";
+                            echo "<strong>{$name}</strong><br>";
                             echo "<div class='pet-image'><img src='{$texturePath}' alt='{$name}'></div>";
-                            echo "<strong>Имя:</strong> {$name}, ";
-                            echo "<strong>Уровень:</strong> {$level}, ";
-                            echo "<strong>Soulbound:</strong> {$soulbound}, ";
-                            echo "<strong>Редкость:</strong> {$rarity}, ";
-                            echo "<strong>Описание:</strong> {$lore}";
+                            echo "<div class='pet-details'>";
+                            echo "{$lore}<br>";
+                            echo "</div>";
                             echo "</div>";
                         }
                         echo "</div>";
